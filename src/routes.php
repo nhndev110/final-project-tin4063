fb31268768ce3b3620a42825ace09a900db07381
huynguyen1611 <huycuteo16112003@gmail.com>
2024-12-25 17:57:27 +0700

    comment

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Controllers\AccountController;
+use App\Controllers\CommentController;
 use App\Controllers\HomeController;
 use App\Controllers\PostController;
 use App\Controllers\SearchController;
@@ -14,10 +15,11 @@ $router->addRoute('/home', [new HomeController(), 'index']);
 $router->addRoute('/post/create', [new PostController(), 'create']);
 $router->addRoute('/post/update/(\d+)', [new PostController(), 'update']);
 $router->addRoute('/post/delete/(\d+)', [new PostController(), 'delete']);
-
 $router->addRoute('/search', [new SearchController(), 'index']);
 
 $router->addRoute('/profile', [new AccountController(), 'profile']);
 $router->addRoute('/login', [new AccountController(), 'login']);
 $router->addRoute('/signup', [new AccountController(), 'signup']);
 $router->addRoute('/logout', [new AccountController(), 'logout']);
+
+$router->addRoute('/post/(\d+)/comment/create', [new CommentController(), 'create']);
